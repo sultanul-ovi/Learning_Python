ebf59bce4ed061feeea7badbb85a7dcb3f6ad3ec
sultanul-ovi <sultanulislamovi@gmail.com>
2023-03-28 06:47:41 +0600

    class 8

---
+++ Automation/tempCodeRunnerFile.py
@@ -0,0 +1,13 @@
+print('Alice')
+# print("This is Alice's cat")
+
+# print('This is Alice\'s cat')
+
+# print('\t tab')
+# print('\nOvi\nOvi')
+# print(r'This is Alice\'s Cat')
+# print("""Dear Alice,
+#         Eve's cat has been arrested for 
+#         catnapping, cat burglary,and extortion.
+#         Sincerely,
+#         Bob.""")
